2a6d498ac33fe7a91f09ab3115270b281e821634
Itumeleng NY <itumelengnyathela@icloud.com>
2026-07-16 19:39:11 +0200

    Finished up the portfolio, added a contact form with validation, added success and error message for the form, removed certifications and added projects

--- systemFunction.js
+++ systemFunction.js
@@ -26,3 +26,111 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 });
+
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    emailjs.init("ahQEPQjGJSR0wItnz");
+
+    const form = document.getElementById("contactForm");
+    const popup = document.getElementById("successPopup");
+    const card = document.getElementById("successCard");
+    const closePopup = document.getElementById("closePopup");
+    const errorPopup = document.getElementById("errorPopup");
+    const errorCard = document.getElementById("errorCard");
+    const closeErrorPopup = document.getElementById("closeErrorPopup");
+
+    if (!form) return;
+
+    // Submit Form
+    form.addEventListener("submit", async (e) => {
+        e.preventDefault();
+        const button = form.querySelector("button");
+        button.disabled = true;
+        button.innerHTML = `
+            <div class="flex items-center justify-center gap-3">
+                <svg class="w-5 h-5 animate-spin"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24">
+                    <circle
+                        class="opacity-25"
+                        cx="12"
+                        cy="12"
+                        r="10"
+                        stroke="currentColor"
+                        stroke-width="4">
+                    </circle>
+                    <path
+                        class="opacity-75"
+                        fill="currentColor"
+                        d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z">
+                    </path>
+                </svg>
+                Sending Message...
+            </div>
+        `;
+        try {
+            await emailjs.send(
+                "service_hrudo3i",
+                "template_azp0inm",
+                {
+                    from_name: document.getElementById("name").value,
+                    from_email: document.getElementById("email").value,
+                    subject: document.getElementById("subject").value,
+                    message: document.getElementById("message").value
+                }
+            );
+            // Show success popup
+            popup.classList.remove("hidden");
+            popup.classList.add("flex");
+            setTimeout(() => {
+                card.classList.remove("scale-90", "opacity-0");
+                card.classList.add("scale-100", "opacity-100");
+            }, 30);
+            form.reset();
+        } catch (error) {
+            console.error("EmailJS Error:", error);
+
+            errorPopup.classList.remove("hidden");
+            errorPopup.classList.add("flex");
+
+            setTimeout(() => {
+                errorCard.classList.remove("scale-90", "opacity-0");
+                errorCard.classList.add("scale-100", "opacity-100");
+            }, 30);
+        } finally {
+            button.disabled = false;
+            button.innerHTML = `
+                Send Message
+                <i class="fas fa-arrow-right"></i>
+            `;
+        }
+    });
+    // Close popup for success
+    if (closePopup) {
+        closePopup.addEventListener("click", () => {
+            card.classList.remove("scale-100", "opacity-100");
+            card.classList.add("scale-90", "opacity-0");
+            setTimeout(() => {
+                popup.classList.remove("flex");
+                popup.classList.add("hidden");
+            }, 300);
+        });
+    }
+
+    // Close popup for error
+    if (closeErrorPopup) {
+    closeErrorPopup.addEventListener("click", () => {
+
+        errorCard.classList.remove("scale-100", "opacity-100");
+        errorCard.classList.add("scale-90", "opacity-0");
+
+        setTimeout(() => {
+            errorPopup.classList.remove("flex");
+            errorPopup.classList.add("hidden");
+        }, 300);
+
+    });
+}
+});
