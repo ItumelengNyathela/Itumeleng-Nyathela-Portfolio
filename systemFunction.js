8e9730e3fb984f087f2a6c946ccbb637cdf1c486
Itumeleng NY <itumelengnyathela@icloud.com>
2026-07-17 14:34:17 +0200

    Fixed the email receiver, to be able to match EmailJS variables the the JS script

--- systemFunction.js
+++ systemFunction.js
@@ -75,8 +75,8 @@ document.addEventListener("DOMContentLoaded", () => {
                 "service_hrudo3i",
                 "template_azp0inm",
                 {
-                    from_name: document.getElementById("name").value,
-                    from_email: document.getElementById("email").value,
+                    name: document.getElementById("name").value,
+                    email: document.getElementById("email").value,
                     subject: document.getElementById("subject").value,
                     message: document.getElementById("message").value
                 }
